63ea1152f88307521df9b2e59bf738757e060d22
cloud / Devops engineer <98840383+ZayaniHassen@users.noreply.github.com>
2023-11-02 02:14:39 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false ,
-  apiUrl: 'http://192.168.162.222:8089',
+  production: false
 };
 
 /*
